02b9b6e52af8c5cf6ed0c9dc08e200f6e18c0306
soomin <lsm798@naver.com>
2022-02-22 09:05:37 +0900

    second commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-const ArrowButton = document.querySelector(".pagelink > p");
-// console.log(ArrowButton);
-ArrowButton.addEventListener("click", subpagelink);
+const btn_top = document.querySelector(".btn_top");
+const btn_bottom = document.querySelector(".btn_bottom");
+const img_wrap = document.querySelector(".product");
+const img_lst = document.getElementsByClassName("product_lst");
 
-function subpagelink(){
-    ArrowButton.documentElement.style.setProperty('--arrowdeg','-90deg');
+let move_area = 0;
+
+function btn_top_click() {
+    if (move_area > ((img_lst.length-1) * -200)) {
+        move_area -= 200;
+        img_wrap.style.top = move_area + "px";
+        console.log(move_area);
+    }
+    else {
+        move_area = img_lst.length * 200;
+        img_wrap.style.top = "-" + move_area + "px";
+    }
 }
+// function btn_bottom_click() {
+    
+// }
+
+btn_top.addEventListener("click", btn_top_click);
