c423c6bb201c43eacab3d817d96f67e73d9e1c71
soomong2 <lsm7982@naver.com>
2022-02-22 23:46:57 +0900

    third commit

--- js/index.js
+++ js/index.js
@@ -6,18 +6,26 @@ const img_lst = document.getElementsByClassName("product_lst");
 let move_area = 0;
 
 function btn_top_click() {
-    if (move_area > ((img_lst.length-1) * -200)) {
-        move_area -= 200;
+    move_area -= 200;
+    if (move_area < -(img_lst.length * 200 - 200)) {
+        move_area = -400;
         img_wrap.style.top = move_area + "px";
-        console.log(move_area);
     }
     else {
-        move_area = img_lst.length * 200;
-        img_wrap.style.top = "-" + move_area + "px";
+        img_wrap.style.top = move_area + "px";
     }
 }
-// function btn_bottom_click() {
-    
-// }
+
+function btn_bottom_click() {
+    move_area += 200;
+    if (move_area > 0) {
+        move_area = 0;
+        img_wrap.style.top = move_area + "px";
+    }
+    else {
+        img_wrap.style.top = move_area + "px";
+    }
+}
 
 btn_top.addEventListener("click", btn_top_click);
+btn_bottom.addEventListener("click", btn_bottom_click);
